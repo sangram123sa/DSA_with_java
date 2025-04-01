2e8b5c55e1b65fc01a81d4a4a845bd72166f9b8f
sangram123sa <aparnasamanta90028@gmail.com>
2025-04-02 00:15:44 +0530

    problem for set matrix zeros

--- Arrays/Array.java
+++ Arrays/Array.java
@@ -475,6 +475,96 @@ public class Array {
         }
         return reverseArray(arr, breakingIndex+1, arr.length-1);
     }
+    // make row -1
+    public static void makeRow(int[][] matrix, int row) {
+        for (int i = 0; i < matrix.length; i++) {
+            if (matrix[row][i] != 0) {
+                matrix[row][i] = -1;                            
+            }
+        }
+    }
+    // make column -1
+    public static void makeColumn(int[][] matrix, int col) {
+        for (int i = 0; i < matrix[col].length; i++) {
+            if (matrix[i][col] != 0) {
+                matrix[i][col] = -1;
+            }
+        }
+    }
+    // Set matrix zeros problem 
+    public static int[][] setMatrixZeros(int[][] matrix) {
+        // Brute ->
+        //  for (int i = 0; i < matrix.length; i++) {
+        //     for (int j = 0; j < matrix[0].length; j++) {
+        //         if (matrix[i][j] == 0) {
+        //             makeRow(matrix, i);
+        //             makeColumn(matrix, j);
+        //         }
+        //     }
+        //  }
+        //  for (int i = 0; i < matrix.length; i++) {
+        //     for (int j = 0; j < matrix[i].length; j++) {
+        //         if (matrix[i][j] == -1) {
+        //             matrix[i][j] = 0;
+        //         }
+        //     }
+        //  }
+        //  return matrix;
+
+        // Better ->
+        // int[] row = new int[matrix.length];
+        // int[] col = new int[matrix[0].length];
+        // for (int i = 0; i < matrix.length; i++) {
+        //     for (int j = 0; j < matrix[0].length; j++) {
+        //         if (matrix[i][j] == 0) {
+        //             row[i] = -1;
+        //             col[j] = -1;
+        //         }
+        //     }
+        // }
+        // for (int i = 0; i < row.length; i++) {
+        //     for (int j = 0; j < col.length; j++) {
+        //         if (row[i] == -1 || col[j] == -1) {
+        //             matrix[i][j] = 0;
+        //         }
+        //     }
+        // }
+        // return matrix;
+
+        // optimal
+        int col0 = 1;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    if (j != 0) {
+                        matrix[0][j] = 0;
+                    }else{
+                        col0 = 0;
+                    }
+                }
+            }
+        }
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[0].length; j++) {
+                if (matrix[i][j] != 0 &&(matrix[i][0] == 0 || matrix[0][j] == 0)) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        if (matrix[0][0] == 0) {
+            for (int i = 1; i < matrix.length; i++) {
+                matrix[0][i] = 0;
+            }
+        }
+        if (col0 == 0) {
+            for (int i = 0; i < matrix.length; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+        return matrix;
+    }
     public static void main(String[] args) {
         // int[] arr1 = {5,6,7,9};
         // int[] arr = {0,0,1,1,0,1,1,1,0,1,1};
@@ -483,7 +573,7 @@ public class Array {
         // int arr[] = {2,2,3,3,1,4,4};
         // int arr[] = {-2,-3,4,-1,-2,1,5,-3};
         // int arr[] = {3,-2,-4,1,5,-7};
-        int arr[] = {2,1,5,4,3,0,0};
+        // int arr[] = {2,1,5,4,3,0,0};
         // int k = 5;
         // int k = 5;
         // System.out.println(secondLargestElement(arr));  
@@ -507,8 +597,17 @@ public class Array {
         // for (int i : rearrangeArray(arr)) {
         //     System.out.print(i+" ");
         // }
-        for (int i : nextPermutation(arr)) {
-            System.out.print(i+" ");
+        // for (int i : nextPermutation(arr)) {
+        //     System.out.print(i+" ");
+        // }
+
+        int[][] matrix = {{1,1,1,1}, {1,0,0,1}, {1,1,0,1}, {1,1,1,1}};
+        matrix = setMatrixZeros(matrix);
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j]+" ");
+            }
+            System.out.println(" ");
         }
     }
 }
